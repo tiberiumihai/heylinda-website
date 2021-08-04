0bc97218f3cf67a7c1930d652aa133f32c91d962
Adrian Carolli <adrian.caarolli@gmail.com>
2021-08-04 16:37:05 -0400

    target blank

--- pages/index.js
+++ pages/index.js
@@ -120,6 +120,7 @@ export default function Home() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
+                    target="_blank"
                     href="https://join.slack.com/t/heylinda/shared_invite/zt-to976i3o-qPBT6C~pU0QjyMeMgvMVYQ"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
@@ -128,6 +129,7 @@ export default function Home() {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
+                    target="_blank"
                     href="https://github.com/heylinda/heylinda-app"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo md:py-4 md:text-lg md:px-10"
                   >
