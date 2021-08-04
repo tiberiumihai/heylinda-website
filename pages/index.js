df13a2a00e74ea8f595091ec0075978da2232dc5
Adrian Carolli <adrian.caarolli@gmail.com>
2021-08-04 17:14:43 -0400

    Alt text for images

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,11 @@ export default function Home() {
                     <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                       <div className="px-5 pt-4 flex items-center justify-between">
                         <div>
-                          <img className="h-8 w-auto" src="/logo.png" alt="" />
+                          <img
+                            className="h-8 w-auto"
+                            src="/logo.png"
+                            alt="Girl in lotus position"
+                          />
                         </div>
                         <div className="-mr-2">
                           <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -149,7 +153,7 @@ export default function Home() {
         </div>
       </div>
       <div className="rounded-lg ring-1 ring-gray-900 ring-opacity-5 overflow-hidden bg-white">
-        <img className="hidden sm:block w-full h-auto" src="/hero.jpg" alt="" />
+        <img className="hidden sm:block w-full h-auto" src="/hero.jpg" alt="Meditation app" />
       </div>
     </div>
   )
