ce10243d82a3a24a20fceedd011a31448bff1ef9
Adrian Carolli <adrian.caarolli@gmail.com>
2021-08-04 17:32:14 -0400

    Change wording

--- pages/index.js
+++ pages/index.js
@@ -126,7 +126,7 @@ export default function Home() {
             <main className="mt-10 mx-auto w- px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-center">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block">A free forever</span>{' '}
+                  <span className="block">A 100% free</span>{' '}
                   <span className="block text-indigo-600 xl:inline">meditation app</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
